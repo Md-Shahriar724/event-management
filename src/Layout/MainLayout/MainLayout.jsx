d09947ea8af1731498bf390218ff4cdde76d69e6
Md-Shahriar724 / <mdshahriar724@gmail.com>
2024-04-04 23:05:39 +0600

    REact router and tailwind css integrated and I am useing daisyUI , My navbar already created

--- src/Layout/MainLayout/MainLayout.jsx
+++ src/Layout/MainLayout/MainLayout.jsx
@@ -1,9 +1,12 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../../Components/Navbar/Navbar";
 
 
 const MainLayout = () => {
     return (
         <div>
-            <h1>This is main layout</h1>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
         </div>
     );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,12 @@
 
 const Home = () => {
+    const image = {
+        eve:''
+    }
     return (
         <div>
             <h1>This is home</h1>
+            <img src={image.eve} alt="" />
         </div>
     );
 };
